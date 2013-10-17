0f251d14c622a63f62b0e59c23f82dad293c325d
lei-clearsky <sportzhulei@gmail.com>
2013-10-17 19:44:40 -0400

    Added welcome and stories controllers and views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsyNews::Application.config.secret_key_base = '9ff7c3a5ac54b6ec69ddeb79fdc26d3acb123309d53cc4732eaf4719d4e20b9e05d669ec3f9ea29688c72d7b31a66ea02a2e69c892abbc5859c8b55e6c738ef7'
